6831ad2864a76b4a99ce42c0859223037ffa9c1e
coolcom200 <coolcom200@users.noreply.github.com>
2022-03-07 10:35:23 -0500

    Basic Page done

--- client/src/main/kotlin/screens/editor/EditorScreen.kt
+++ client/src/main/kotlin/screens/editor/EditorScreen.kt
@@ -1,17 +1,17 @@
 package screens.editor
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -19,6 +19,7 @@ import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import controllers.EditorController
 import navcontroller.NavController
@@ -29,30 +30,93 @@ fun EditorScreen(
     editorController: EditorController
 ) {
     var text by rememberSaveable { mutableStateOf("") }
+    var isEditingTitle by remember { mutableStateOf(false) }
+    var currentTitle by remember { mutableStateOf("Untitled") }
+    var edittingTitle by remember { mutableStateOf(currentTitle) }
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(isEditingTitle) {
+        if (isEditingTitle) {
+            focusRequester.requestFocus()
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Note Editor",
-            style = MaterialTheme.typography.h6
-        )
+        TopAppBar(
+            backgroundColor = Color.White,
+            elevation = 0.dp,
+            navigationIcon = {
+                IconButton(onClick = { navController.navigateBack() }) {
+                    Icon(
+                        Icons.Filled.ArrowBack,
+                        contentDescription = "Navigate Back"
+                    )
+                }
+            },
+            title = {
+                if (!isEditingTitle) {
+                    Text(text = currentTitle, style = MaterialTheme.typography.h6)
+                    IconButton(
+                        onClick = {
+                            isEditingTitle = !isEditingTitle
+                        }
+                    ) {
+                        Icon(
+                            Icons.Filled.Edit,
+                            contentDescription = "Edit Title"
+                        )
+                    }
+                } else {
+                    BasicTextField(
+                        edittingTitle,
+                        { edittingTitle = it },
+                        singleLine = true,
+                        maxLines = 1,
+                        textStyle = MaterialTheme.typography.h6,
+                        modifier = Modifier.focusRequester(focusRequester)
+
+                    )
+                    IconButton(onClick = {
+                        currentTitle = edittingTitle
+                        isEditingTitle = false
+                        focusRequester.freeFocus()
+                    }) {
+                        Icon(
+                            Icons.Filled.Done,
+                            contentDescription = "Accept"
+                        )
+                    }
+                    IconButton(onClick = {
+                        isEditingTitle = false
+                    }) {
+                        Icon(
+                            Icons.Filled.Close,
+                            contentDescription = "Discard"
+                        )
+                    }
+
+                }
+            })
+        Spacer(Modifier.height(2.dp))
+
+
+        Column(Modifier.padding(16.dp, 0.dp)) {
+            Text("Created by: Leon Fattakhov")
+            Text("Created at: Jan 26, 2022 11:34")
+            Text("Tags: ")
+        }
+
 
         BasicTextField(
+            modifier = Modifier.padding(16.dp, 1.dp).fillMaxWidth().fillMaxHeight(),
             value = text,
             onValueChange = { text = it },
             maxLines = Int.MAX_VALUE,
+            textStyle = MaterialTheme.typography.body1,
             visualTransformation = ColorsTransformation(editorController)
         )
-        Button(
-            onClick = {
-                navController.navigateBack()
-            }
-        ) {
-            Text(
-                text = "Close note"
-            )
-        }
     }
 }
 

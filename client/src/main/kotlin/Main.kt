c493ee3b25bbeaaff32c25051c4b78a2b2918a4e
coolcom200 <coolcom200@users.noreply.github.com>
2022-03-08 16:14:08 -0500

    Show Error Page when server cannot be reached

--- client/src/main/kotlin/Main.kt
+++ client/src/main/kotlin/Main.kt
@@ -1,17 +1,21 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import components.ConnectionError
 import controllers.Authentication
 import controllers.EditorController
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import navcontroller.NavController
 import navcontroller.NavigationHost
@@ -21,6 +25,7 @@ import screens.canvas.CanvasScreen
 import screens.editor.EditorScreen
 import screens.login.LoginScreen
 import screens.login.SignUpScreen
+import kotlin.time.Duration.Companion.seconds
 
 val interFontFamily = FontFamily(
     Font(
@@ -44,30 +49,58 @@ val interFontFamily = FontFamily(
 @Preview
 fun App(authenticated: Boolean) {
     val screens = Screen.values().toList()
-    val navController by rememberNavController( if (authenticated) Screen.CanvasScreen.name else Screen.LoginScreen.name )
+    val navController by rememberNavController(if (authenticated) Screen.CanvasScreen.name else Screen.LoginScreen.name)
     val currentScreen by remember {
         navController.currentScreen
     }
 
-    MaterialTheme {
-        Router(navController = navController)
-    }
+    Router(navController = navController)
 }
 
 fun main() = application {
     PrivateJSONToken.loadJWTFromAppData()
     val jwt = PrivateJSONToken.token
+
+    var canConnect by remember { mutableStateOf(false) }
+    val connectivityChecker = connectionMonitor()
+    val scope = rememberCoroutineScope()
+
     val isJWTValid = jwt.isNotBlank() &&
             runBlocking {
-                Authentication.isJWTValid(jwt)
+                canConnect = nHttpClient.canConnectToServer()
+                canConnect && Authentication.isJWTValid(jwt)
             }
+
+
+
+    scope.launch {
+        connectivityChecker.collect { value ->
+            canConnect = value
+        }
+    }
 
     Window(
         title = ResString.appName,
         onCloseRequest = ::exitApplication
     ) {
-        App(isJWTValid)
+        MaterialTheme {
+            if (canConnect) {
+                App(isJWTValid)
+            } else {
+                ConnectionError()
+            }
+        }
     }
+}
+
+private fun connectionMonitor(): Flow<Boolean> {
+    val connectivityChecker = flow {
+        while (true) {
+            emit(nHttpClient.canConnectToServer())
+            delay(75.seconds.inWholeMilliseconds)
+        }
+    }
+    return connectivityChecker
 }
 
 enum class Screen() {

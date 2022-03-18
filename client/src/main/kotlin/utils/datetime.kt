03434c27ad97ef20dfc2cf71da0b74d9db41eb57
Kelvin Zhang <me@kelvinzhang.ca>
2022-03-18 10:51:50 -0400

    better time formatting

--- client/src/main/kotlin/utils/datetime.kt
+++ client/src/main/kotlin/utils/datetime.kt
@@ -1,17 +1,21 @@
 package utils
 
-import java.text.SimpleDateFormat
-import java.util.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 // take in a datetime string as 2022-03-16T00:00:00.000 and return a string of the form
 // 16 March 2022, 00:00 AM/PM
 fun formatDateTime(datetime: String): String {
     print(datetime)
-    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.ENGLISH)
-    val date = sdf.parse(datetime)
-    val sdf2 = SimpleDateFormat("d MMM yyyy 'at' h:mm a", Locale.ENGLISH)
-    return sdf2.format(date)
+    val formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a")
+    try {
+        val date = LocalDateTime
+            .parse(datetime)
+        return date.format(formatter)
+    } catch (e: Exception) {
+        return datetime
+    }
 }
 
 
